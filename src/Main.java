44df0aea0cc95cc6fc262620b2b98cc3b8a9b764
GarrisonMoore <mooreg444@gmail.com>
2025-12-10 23:39:31 -0800

    Refactored `Main` class structure and added build artifacts for JAR creation.

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,23 @@
 
 import javax.swing.*;
 
-void main() {
-    // using a modern-looking UI format (Nimbus)
-    try {
-        // Set Nimbus as the UI manager for the application
-        for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-            // if Nimbus is available, use it
-            if ("Nimbus".equals(info.getName())) {
-                UIManager.setLookAndFeel(info.getClassName());
-                break;
+public class Main {
+    public static void main(String[] args) {
+        // using a modern-looking UI format (Nimbus)
+        try {
+            // Set Nimbus as the UI manager for the application
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                // if Nimbus is available, use it
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
             }
+        } catch (Exception e) {
+            // If Nimbus isn't available, keep the default
         }
-    } catch (Exception e) {
-        // If Nimbus isn't available, keep the default
-    }
 
-    // Create the UI on the Event Dispatch Thread
-    SwingUtilities.invokeLater(WhiteBoard::new);
+        // Create the UI on the Event Dispatch Thread
+        SwingUtilities.invokeLater(WhiteBoard::new);
+    }
 }
